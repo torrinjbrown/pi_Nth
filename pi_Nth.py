cb1d4c64ac1e2479997dc6c7d3614b87e1e55b66
Torrin <38574537+torrinjbrown@users.noreply.github.com>
2023-05-08 06:16:56 -0400

    Add files via upload

--- pi_Nth.py
+++ pi_Nth.py
@@ -7,19 +7,21 @@ user_answer = 'yes'
 user = input("Would you like to round pi the Nth digit: ")
 
 #Set a condition if the user doesnt answer
-while user.lower() != user_answer: 
-   user = input("Would you like to round pi the Nth digit: ")
+while True:
+     #Creating a base input for the user
+     user = input("Would you like to round pi the Nth digit: ")
+     pi = float
+     if user.lower() == 'yes':
+         #Setting variable input for the user to ask 
+         pi = int(input("How many spaces, of pi do you want to see?(Max 50): "))
 
-if user.lower() == 'no':
-    print('Hope you have a great day')
-
-elif user.lower() == 'yes':
-  #Setting variable input for the user to ask 
-  pi = int(input("How many spaces, of pi do you want to see?: "))
-
-#Creating a codition for if variable is greater than set number
-while pi > 50:
-	print("The number you type was too high. Please type something lower than 50")
-	precision = int(raw_input("How many spaces, of pi do you want to see?: "))
-else:
-	print('%.*f' % (pi, math.pi))
+         #Creating a codition for if variable is greater than set number
+         while pi > 50:
+             print("The number you type was too high. Please type something lower than 50")
+             pi = int(input("How many spaces, of pi do you want to see?: "))
+         else:
+             print('%.*f' % (pi, math.pi))
+             break
+    
+     else:
+         print('Hope you have a great day')
